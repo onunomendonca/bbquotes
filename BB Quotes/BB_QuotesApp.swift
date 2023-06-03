16284040556850437e5541948d4637b4f5733bff
Nuno Mendonça <nunomendonca95@gmail.com>
2023-06-03 16:15:13 +0100

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Nuno Mendonça on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
